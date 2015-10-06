9eef2ffb5a331d2c88c3bdaf8936c7298fbdc3ba
Massimiliano Mantione <massimiliano.mantione@gmail.com>
2015-10-06 16:29:21 +0200

    Simple GC implementation.

--- test/test-operations.mjs
+++ test/test-operations.mjs
@@ -5,12 +5,15 @@ var
   expect = (require 'chai').expect
   mock-fs = require 'mock-fs'
   directory-repo = require '../lib/directory-repo'
+  hash = require '../lib/hash'
   archive = require '../lib/archive'
   checkout = require '../lib/checkout'
   pull = require '../lib/pull'
   copy = require '../lib/copy'
   trim = require '../lib/trim'
   sync = require '../lib/sync'
+  gc = require '../lib/gc'
+  chk = require '../lib/check'
 
 var check = (fs, path, kind) ->
   try do
@@ -25,17 +28,40 @@ var check = (fs, path, kind) ->
     false
 
 var
-  dir2-hash = 'D-91168c13aa4283bb352aee5251d08f54abc255ad'
-  dir3-hash = 'D-cad9d91ad47f5348e6573e5d25814e30db8e4c52'
-  f3-hash = 'F-619aae029dda528253a6af0ba619b45baa1df115'
-  f4-hash = 'F-adfec5772ae8932aa10896037b0779bec915015b'
-  same-hash = 'F-ff3390557335ba88d37755e41514beb03bc499ec'
-  dir2-data = [
-    'd:D-cad9d91ad47f5348e6573e5d25814e30db8e4c52:dir3'
-    'f:F-619aae029dda528253a6af0ba619b45baa1df115:f3.txt'
-    'f:F-adfec5772ae8932aa10896037b0779bec915015b:f4.txt'
-    'f:F-ff3390557335ba88d37755e41514beb03bc499ec:same.txt'].join '/'
+  f1-hash = hash.sync('f1', 'f')
+  f2-hash = hash.sync('f2', 'f')
+  f3-hash = hash.sync('f3', 'f')
+  f4-hash = hash.sync('f4', 'f')
+  f5-hash = hash.sync('f5', 'f')
+  f6-hash = hash.sync('f6', 'f')
+  same-hash = hash.sync('same', 'f')
+  f1-data = ['f', f1-hash, 'f1.txt'].join ':'
+  f2-data = ['f', f2-hash, 'f2.txt'].join ':'
+  f3-data = ['f', f3-hash, 'f3.txt'].join ':'
+  f4-data = ['f', f4-hash, 'f4.txt'].join ':'
+  f5-data = ['f', f5-hash, 'f5.txt'].join ':'
+  f6-data = ['f', f6-hash, 'f6.txt'].join ':'
+  same-data = ['f', same-hash, 'same.txt'].join ':'
+  dir2-3-text = [
+      f5-data
+      f6-data
+    ].sort().join '/'
+  dir2-3-hash = hash.sync(dir2-3-text, 'd')
+  dir2-text = [
+      f3-data
+      f4-data
+      same-data
+      ['d', dir2-3-hash, 'dir3'].join ':'
+    ].sort().join '/'
+  dir2-hash = hash.sync(dir2-text, 'd')
+  dir3-text = [
+      f1-data
+      f3-data
+      same-data
+    ].sort().join '/'
+  dir3-hash = hash.sync(dir3-text, 'd')
   dir2-tag = 'DIR2-v1'
+  dir2-tag-hash = hash.apply-kind('t', 'DIR2-v1')
 
 describe
   "Operations"
@@ -61,7 +87,7 @@ describe
           .then #->
             expect(#it).to.equal dir2-hash
             expect(file? ('/repo/' + dir2-hash)).to.equal true
-            expect(contents ('/repo/' + dir2-hash)).to.equal dir2-data
+            expect(contents ('/repo/' + dir2-hash)).to.equal dir2-text
             expect(contents ('/repo/' + 'T-' + dir2-tag)).to.equal dir2-hash
             done()
           .catch #-> done #it
@@ -98,7 +124,7 @@ describe
           .then #->
             Promise.all ([
               repo2.check dir2-hash
-              repo2.check dir3-hash
+              repo2.check dir2-3-hash
               repo2.check f3-hash
               repo2.check f4-hash
               repo2.check same-hash
@@ -127,7 +153,7 @@ describe
           .then #->
             Promise.all ([
               repo2.check dir2-hash
-              repo2.check dir3-hash
+              repo2.check dir2-3-hash
               repo2.check f3-hash
               repo2.check f4-hash
               repo2.check same-hash
@@ -189,6 +215,35 @@ describe
               true
               true
               false
+              true
+            ])
+            done()
+          .catch #-> done #it
+
+    it
+      'GC a repo'
+      done ->
+        var repo = repo-builder '/repo'
+        |:
+          archive (repo, '/dir2', dir2-tag, fs)
+          .then #->
+            archive (repo, '/dir3', 'DIR3', fs)
+          .then #->
+            repo.remove (dir2-tag-hash)
+          .then #->
+            gc (repo)
+          .then #->
+            Promise.all ([
+              repo.check f4-hash
+              repo.check dir2-hash
+              repo.check dir3-hash
+              repo.check same-hash
+            ])
+          .then #->
+            expect(#it).to.eql ([
+              false
+              false
+              true
               true
             ])
             done()
